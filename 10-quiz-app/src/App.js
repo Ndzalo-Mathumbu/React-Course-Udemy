3f830da7b50d67d98f22817f81e7adf99afbb5e6
Ndzalo-Mathumbu <ndzalonkmathumbu@gmail.com>
2026-02-17 21:54:14 +0200

    created and API to get quiz questions

--- 10-quiz-app/src/App.js
+++ 10-quiz-app/src/App.js
@@ -1,9 +1,48 @@
-import DateCounter from "./DateCounter";
+import Header from "./Header";
+import Main from "./Main";
+import { useEffect, useReducer } from "react";
+
+const initialState = {
+  questions: [],
+  status: "loading",
+};
+
+const reducer = function (CurrentState, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...CurrentState, questions: action.payLoad, status: "ready" };
+    case "dataFailed":
+      return { ...CurrentState, status: "Error" };
+    default:
+      throw new Error("Unknown action");
+  }
+};
 
 const App = function () {
+  const [stateVariable, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    const getQuestions = async function () {
+      try {
+        const res = await fetch("http://localhost:8000/questions");
+        const data = await res.json();
+        dispatch({ type: "dataReceived", payLoad: data });
+      } catch (err) {
+        dispatch({ type: "dataFailed" });
+      }
+    };
+    getQuestions();
+  }, []);
+
   return (
     <>
-      <DateCounter />
+      <div className="app">
+        <Header />
+      </div>
+      <Main>
+        <p>1/15</p>
+        <p>Question</p>
+      </Main>
     </>
   );
 };
