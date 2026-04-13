17930206aaec3b0f3c6e6baa4556ebbfb1bf3a65
Ndzalo-Mathumbu <ndzalonkmathumbu@gmail.com>
2026-04-13 22:20:56 +0200

    installed dependencies to start a new project.

---
+++ 16-react-pizza/eslint.config.js
@@ -0,0 +1,40 @@
+import js from "@eslint/js";
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+import pluginReactHooks from "eslint-plugin-react-hooks";
+
+export default [
+  js.configs.recommended,
+
+  // 1) Load React's recommended rules first
+  pluginReact.configs.flat.recommended,
+
+  // 2) Then apply overrides last (so they win)
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        Intl: "readonly",
+      },
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+      },
+    },
+    plugins: {
+      react: pluginReact,
+      "react-hooks": pluginReactHooks,
+    },
+    settings: {
+      react: { version: "detect" },
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-uses-react": "off",
+      "react/prop-types": "off",
+      "no-unused-vars": "warn",
+    },
+  },
+];
---
+++ 16-react-pizza/src/App.jsx
@@ -0,0 +1,6 @@
+const App = function () {
+  const home = 67;
+  return <h1>Hello World{home}</h1>;
+};
+
+export default App;
