71406f8efa8a1b9945f6a86ce3c89f2579a19006
Ndzalo-Mathumbu <ndzalonkmathumbu@gmail.com>
2026-04-19 19:55:53 +0200

    Add responsive UI styling and layout using Tailwind CSS

--- 16-react-pizza/src/Features/Order/Order.jsx
+++ 16-react-pizza/src/Features/Order/Order.jsx
@@ -1,11 +1,11 @@
 // Test ID: IIDSAT
-
-import { useLoaderData } from "react-router-dom";
+import OrderItem from './OrderItem';
+import { useLoaderData } from 'react-router-dom';
 import {
   calcMinutesLeft,
   formatCurrency,
   formatDate,
-} from "../../Utilities/helpers";
+} from '../../Utilities/helpers';
 
 function Order() {
   const order = useLoaderData();
@@ -20,32 +20,56 @@ function Order() {
     estimatedDelivery,
     cart,
   } = order;
+
   const deliveryIn = calcMinutesLeft(estimatedDelivery);
-
   return (
-    <div>
-      <div>
-        <h2>Status</h2>
+    <div className="py-6 px-4 space-y-8">
+      <div className="flex flex-wrap items-center justify-between ">
+        <h2 className="text-xl font-semibold mb-5"> Order #{id} status</h2>
 
-        <div>
-          {priority && <span>Priority</span>}
-          <span>{status} order</span>
+        <div className="space-x-2 ">
+          {priority && (
+            <span className="rounded-full bg-red-100 border-[1.2px] border-red-600 px-1 py-1 font-medium text-stone-600 tracking-wide">
+              Priority
+            </span>
+          )}
+
+          <span
+            className={`${status === 'delivered' ? 'rounded-full bg-green-100 border-[1.2px] border-green-600 px-1 py-1 font-medium text-stone-600 tracking-wide uppercase' : 'rounded-full bg-orange-100 border-[1.2px] border-orange-500 px-1 py-1 font-medium text-stone-600 tracking-wide uppercase'}`}
+          >
+            order is {status}
+          </span>
         </div>
       </div>
 
-      <div>
+      <div className="flex flex-wrap items-center justify-between gap-2 bg-orange-100 rounded-md py-5 px-6 border-[1px] border-orange-200">
         <p>
           {deliveryIn >= 0
             ? `Only ${calcMinutesLeft(estimatedDelivery)} minutes left 😃`
-            : "Order should have arrived"}
+            : 'Order should have arrived'}
         </p>
-        <p>(Estimated delivery: {formatDate(estimatedDelivery)})</p>
+        <p className="text-xs ">
+          (Estimated delivery: {formatDate(estimatedDelivery)})
+        </p>
       </div>
 
-      <div>
-        <p>Price pizza: {formatCurrency(orderPrice)}</p>
-        {priority && <p>Price priority: {formatCurrency(priorityPrice)}</p>}
-        <p>To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}</p>
+      <ul className="divide-y divide-orange-200 border-b border-orange-200  border-t">
+        {cart.map((item) => (
+          <OrderItem item={item} key={item.id} />
+        ))}
+      </ul>
+      <div className="bg-orange-100 rounded-md py-5 px-6 border-[1px] border-orange-200">
+        <p className="text-sm font-medium">
+          Price pizza: {formatCurrency(orderPrice)}
+        </p>
+        {priority && (
+          <p className="text-sm font-medium">
+            Price priority: {formatCurrency(priorityPrice)}
+          </p>
+        )}
+        <p className="text-sm font-bold">
+          To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}
+        </p>
       </div>
     </div>
   );
--- 16-react-pizza/src/Features/Order/OrderItem.jsx
+++ 16-react-pizza/src/Features/Order/OrderItem.jsx
@@ -1,15 +1,15 @@
-import { formatCurrency } from "../../Utilities/helpers";
+import { formatCurrency } from '../../Utilities/helpers';
 
 function OrderItem({ item, isLoadingIngredients, ingredients }) {
   const { quantity, name, totalPrice } = item;
 
   return (
-    <li>
-      <div>
-        <p>
+    <li className="py-3">
+      <div className="flex items-center justify-between">
+        <p className="font-medium tracking-wide">
           <span>{quantity}&times;</span> {name}
         </p>
-        <p>{formatCurrency(totalPrice)}</p>
+        <p className="text-xs font-medium ">{formatCurrency(totalPrice)}</p>
       </div>
     </li>
   );
