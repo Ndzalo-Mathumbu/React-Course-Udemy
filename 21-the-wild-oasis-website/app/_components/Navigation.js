6de1f8a0b717c93e73f3d888ecc07737e9835fb9
Ndzalo-Mathumbu <ndzalonkmathumbu@gmail.com>
2026-06-13 22:29:03 +0200

    Add active navigation states and cabin text expander

--- 21-the-wild-oasis-website/app/_components/Navigation.js
+++ 21-the-wild-oasis-website/app/_components/Navigation.js
@@ -1,33 +1,38 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Navigation = function () {
+  const links = [
+    { href: "/cabins", label: "Cabins" },
+    { href: "/about", label: "About" },
+    { href: "/account", label: "Guest area" },
+  ];
+
+  const pathname = usePathname();
+
+  console.log(pathname);
+
   return (
     <nav className="z-10 md:text-xl ">
       <ul className="flex md:gap-16 items-center">
-        <li>
-          <Link
-            href="/cabins"
-            className="hover:text-accent-400 transition-colors"
-          >
-            Cabins
-          </Link>
-        </li>
-        <li>
-          <Link
-            href="/about"
-            className="hover:text-accent-400 transition-colors"
-          >
-            About
-          </Link>
-        </li>
-        <li>
-          <Link
-            href="/account"
-            className="hover:text-accent-400 transition-colors"
-          >
-            Guest area
-          </Link>
-        </li>
+        {links.map((link) => (
+          <li key={link.label}>
+            {
+              <Link
+                href={link.href}
+                className={
+                  pathname === link.href || pathname.startsWith(`${link.href}/`)
+                    ? `text-accent-600`
+                    : `hover:text-accent-400 transition-colors`
+                }
+              >
+                {link.label}
+              </Link>
+            }
+          </li>
+        ))}
       </ul>
     </nav>
   );
